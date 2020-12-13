6a1d766ebb4a4162fb90c09179f37e685c6c08f0
daimonz01 <44843350+daimonz01@users.noreply.github.com>
2020-12-14 05:50:20 +0700

    Add files via upload

---
+++ tst.js
@@ -0,0 +1,16 @@
+<div id="videoHouse_External"></div>
+<script type="text/javascript" language="javascript">
+    jwplayer.key = "wNKKFYnF87vWau5d2CtlZTFQMTS/eUlbgRbWDL4JaoI=";
+    jwplayer("videoHouse_External").setup({
+        "file": "https://dl.dropbox.com/s/dqsxztz46hpxecd/CB.m3u8?dl=0",
+        "title": "[Live Streaming TV - CTV Banten] - Nonton Live Streaming TV CTV Banten di TV Stream PF, Nonton TV Online Streaming lengkap dari channel indonesai, internasional, dan premium nonton secara gratis",
+        "image": "https://1.bp.blogspot.com/-gR8d0pMpXgU/XuEjAeydpiI/AAAAAAAABRE/LcieUn0gMf0gtq_36p1-UrrvA0NB_RHCgCK4BGAsYHg/s784/Live%2BStreaming%2BTV%2B-%2BCTV%2BBanten.jpg",
+        "aspectratio": "16:9",
+        "width": "100%",
+        "primary": "html5",
+        "hlshtml": true,
+         "autostart": false,
+         "mute": true
+    });
+
+</script>
